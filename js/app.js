8620de59156da567e32d55ecb3ea9483eaea3a6f
teemopower <jimmykim925@gmail.com>
2016-09-15 22:18:00 -0700

    updated solution

--- js/app.js
+++ js/app.js
@@ -4,3 +4,36 @@
 console.log('App Started');
 console.log('Fruit count', fruits.length);
 console.log('Veggie count', vegetables.length);
+var fruitVeggieApp = angular.module('fruitVeggieApp', []);
+
+ 
+ fruitVeggieApp.controller('FruitVeggieCtrl', ['$scope', function($scope) {
+ 
+  $scope.moveLeft = function(index) {
+     $scope.fruitList.push($scope.produceList[index]);
+     $scope.produceList.splice(index, 1);
+   }
+ 
+   $scope.moveRight = function(index) {
+     $scope.veggieList.push($scope.produceList[index]);
+     $scope.produceList.splice(index, 1);
+   }
+ 
+   $scope.produceList = [
+   'Artichoke',
+   'Apricot',
+   'Apple',
+   'Banana',
+   'Blackberry',
+   'Blueberry',
+   'Currant',
+   'Cherry',
+   'Gooseberry',
+   'Grapefruit',
+   'Kumquat'
+ ];
+ 
+ $scope.veggieList = [];
+ $scope.fruitList = [];
+ 
+ }]);
